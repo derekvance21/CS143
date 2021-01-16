8877892c905b15ac67cd9c0423bec6d640106cef
Derek Vance <derekvance21@gmail.com>
2021-01-16 19:30:26 +0000

    Fix q2.sql

--- project-1/q2.sql
+++ project-1/q2.sql
@@ -1,9 +1,3 @@
--- SELECT first, last
--- FROM Actor
--- INNER JOIN MovieActor ON Actor.id=aid
--- INNER JOIN Movie ON mid=Movie.id 
--- WHERE title="Die Another Day";
-
 SELECT first, last
 FROM Actor, MovieActor, Movie
 WHERE Actor.id=aid 
